b2f812cb6e90967c34e9d6ccb648af6ffb8eda60
gregk27 <git@gregk.ca>
2020-10-31 16:46:03 -0400

    Converted Event to class, added function to get string representation

--- game.h
+++ game.h
@@ -16,32 +16,36 @@ enum class Alliance {
     RED, BLUE, NEUTRAL
 };
 
-// Types of events that can be logged
-enum class EventType {
-    // Robot passed through a node
-    PASSTHROUGH,
-    // Robot crossed a defense
-    CROSS,
-    // Robot intaked a ball
-    INTAKE,
-    // Robot scored a high goal
-    SCORE_HIGH,
-    // Robot scored a low goal
-    SCORE_LOW
-};
-
 // Structure represnting a simulation event
-struct Event {
-    // Robot that made the event
-    Robot *r;
-    // Location of the event
-    Fieldpoint *location;
-    // Type of the event
-    EventType type;
-    // Point change from the event
-    int points;
-    // Timestamp of the event, in seconds
-    int time;
+class Event {
+    public:
+        // Types of events that can be logged
+        enum class Type {
+            // Robot passed through a node
+            PASSTHROUGH,
+            // Robot crossed a defense
+            CROSS,
+            // Robot intaked a ball
+            INTAKE,
+            // Robot scored a high goal
+            SCORE_HIGH,
+            // Robot scored a low goal
+            SCORE_LOW
+        };
+
+
+        // Robot that made the event
+        Robot *r;
+        // Location of the event
+        Fieldpoint *location;
+        // Type of the event
+        Type type;
+        // Point change from the event
+        int points;
+        // Timestamp of the event, in seconds
+        int time;
+
+        std::string toString();
 };
 
 #endif // !GAME_H`
