d3f25cf27336adb5e54621bdafb066a4d21dde17
gregk27 <git@gregk.ca>
2020-11-10 00:33:42 -0500

    Revert "Removed various SDL_GFX files which were unnecessary (mostly project/build/testing)"
    This reverts commit 43fcd030997d9f0497e48e5a76b7b55423ecbf96.

--- UI/Interface.cpp
+++ UI/Interface.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <stdbool.h>
 #include "interface.h"
-#include "../SDL_GFX/SDL_gfxPrimitives.h"
 #include "../SDL2/SDL.h"
 #include "../SDL2/SDL_render.h"
 #include "../SDL2/SDL_rect.h"
---
+++ main.cpp
@@ -0,0 +1,49 @@
+// FLAGS: -ISDL2 -LSDL2 -lmingw32 -lSDL2main -lSDL2
+#include <stdio.h>
+#include <stdbool.h>
+#include <iostream>
+#include <thread>
+#include "robot.h"
+// #include "field/Fieldpoint.h"
+#include "field/field.h"
+#include "utils/LinkedList.h"
+#include "utils/Graph.h"
+#include "game.h"
+#include "ui/Interface.h"
+
+int main(int argc, char *argv[]){
+
+    printf("Hello World\n");
+
+    std::thread renderThread(Interface::init);
+
+    Robot *r;
+    r = Robot::parseCSV("./robots.csv");
+    // // Event queue to be populated by simulation
+    // LinkedList<Event> events;
+
+    // Defense *d = new Defense(0,0);
+    // for(int i=0;i<8;i++){
+    //     d->defType = (Defense::Defenses) i;
+    //     printf("%d\n", r->crossTime(d));
+    // }
+
+    // Field::print(r->graph, false);
+    Field::toGraphML(r->graph, "out.graphml");
+    Interface::setGraph(r->graph);
+    // Interface::drawGraph(r->graph);
+
+    // // print_bin(r.defenses, 16);
+
+    // // printf("%d, %d, %d, %d\n",
+    // // r.defenses,
+    // // can_cross(&r, PORTCULLIS),
+    // // can_cross(&r, SALLY_PORT), 
+    // // can_cross(&r, ROCK_WALL));
+    // print_field(true);
+
+    // std::cout << "/* message */" << std::endl;
+    
+    renderThread.join();
+    return 0;
+}
