44dc2d2760d39e51e0f6604b620d47915f967c8a
gregk27 <git@gregk.ca>
2020-11-23 23:15:56 -0500

    Added simple scrollable menu for events

--- UI/Console.cpp
+++ UI/Console.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <time.h>
 
 #include "Console.h"
 #include "../game.h"
@@ -14,44 +15,75 @@
 #define KEY_RIGHT 77
 #define KEY_ESC 27
 
+#define HEIGHT 30
+#define LINES 25
+
 LinkedList<Event> *events;
 
+HANDLE handle;
 
 void setCursor(short x, short y){
-    SetConsoleCursorPosition( GetStdHandle (STD_OUTPUT_HANDLE), {x,y});
+    SetConsoleCursorPosition(handle, {x,y});
 }
 
 void Console::setEvents(LinkedList<Event> *e){
     events = e;
 }
 
+void redraw(int selected){
+    // Reset to top
+    setCursor(0,0);
+    // Print header
+    printf("  Time   Robot @Node    Desc\t\t\t\tPress ESC to exit\n");
+    
+    // Print events
+    for(auto i : (*events)){
+        // Check that it should be on screen
+        // If it's less than the selected value (+ offset) and it's not low enough for bottom, don't render
+        if(i.index < selected-2 && i.index < events->size()-LINES-1){
+            continue;
+        // If it's too low, stop here
+        } else if (i.index > (selected-2 + LINES)){
+            break;
+        }
+        // Get data string
+        std::string out = i.data.toString();
+        // If it's selected, print indicator and colour
+        if(i.index == selected){
+            SetConsoleTextAttribute(handle, 0x0B);
+            printf(" >%s", out.c_str());
+            SetConsoleTextAttribute(handle, 0x0F);
+        } else {
+            printf("  %s", out.c_str());
+        }
+    }
+}
+
 void Console::begin(){
     printf("Hello World\n");
     printf("Printed a thing!\n");
 
+    int selected = 0;
+    handle = GetStdHandle (STD_OUTPUT_HANDLE);
+
     system(CLEAR_COMMAND);
-    for(auto i : (*events)){
-        std::string out = i.data.toString(); 
-        printf("  %s", out.c_str());
-    }
-    setCursor(0,0);
-    printf(">");
+    redraw(selected);
+    // setCursor(0,0);
+    // printf(">");
     int c = 0;
-    int y = 0;
+    // int y = 0;
     while((c=getch()) != 27){
-        setCursor(0,y);
         printf(" ");
         switch (c) {
             case KEY_UP:
-                y --;
+                selected --;
                 break;
             case KEY_DOWN:
-                y ++;
+                selected ++;
                 break;
         }
-        if(y < 0) y=0;
-        if(y >= events->size()) y = events->size()-1;
-        setCursor(0,y);
-        printf(">");
+        if(selected < 0) selected=0;
+        if(selected >= events->size()) selected = events->size()-1;
+        redraw(selected);
     }
 }
