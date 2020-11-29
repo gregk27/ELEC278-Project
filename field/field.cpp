ab2168578498a33b830c2b6a784b8a6fb62fcaf5
gregk27 <git@gregk.ca>
2020-11-29 16:58:22 -0500

    Documented field source and header

--- field/field.cpp
+++ field/field.cpp
@@ -1,3 +1,16 @@
+/**
+ * field.cpp/h
+ * These files contains the the code used to generate the field graph
+ * Pointers to these variables used throughout code for navigation
+ * 
+ * Constant values defined in header are derived from official field drawings, with small tweaks to improve positioning in GUI
+ * All values are in inches, as those are the provided units
+ * 
+ * The following drawing was used in addition to the Game Manual
+ * https://i.imgur.com/1d51oAV.png
+ * https://firstfrc.blob.core.windows.net/frc2016manuals/GameManual/FRC-2016-game-manual-02.pdf 
+ */
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <vector>
@@ -8,6 +21,11 @@
 
 namespace Field {
 
+    // Create nodes based on various constants from header file
+
+    /**
+     * RED NODES
+     */
     Fieldpoint redTowerTop = Fieldpoint(
         RED_TOWER_X+TOWER_OFFSET_X, 
         RED_TOWER_Y+TOWER_OFFSET_Y,
@@ -102,6 +120,9 @@ namespace Field {
         )
     };
 
+    /**
+     * BLUE NODES
+     */
     Fieldpoint blueTowerTop = Fieldpoint(
         BLUE_TOWER_X-TOWER_OFFSET_X,
         BLUE_TOWER_Y+TOWER_OFFSET_Y,
@@ -196,6 +217,9 @@ namespace Field {
         )
     };
 
+    /**
+     * NEUTRAL
+     */
     Fieldpoint centreBalls[CENTRE_BALL_COUNT] = {
         Fieldpoint(
             FIELD_LENGTH/2,
@@ -359,12 +383,15 @@ namespace Field {
 
     void toGraphML(Graph *g, const char* filename){
         FILE *f = fopen(filename, "w");
+        // Add config lines taken from exported file
         fprintf(f, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n<!--Created by yFiles for HTML 2.3.0.3-->\n<graphml xsi:schemaLocation=\"http://graphml.graphdrawing.org/xmlns http://www.yworks.com/xml/schema/graphml.html/2.0/ygraphml.xsd \" xmlns=\"http://graphml.graphdrawing.org/xmlns\" xmlns:demostyle=\"http://www.yworks.com/yFilesHTML/demos/FlatDemoStyle/1.0\" xmlns:icon-style=\"http://www.yworks.com/yed-live/icon-style/1.0\" xmlns:bpmn=\"http://www.yworks.com/xml/yfiles-bpmn/2.0\" xmlns:demotablestyle=\"http://www.yworks.com/yFilesHTML/demos/FlatDemoTableStyle/1.0\" xmlns:uml=\"http://www.yworks.com/yFilesHTML/demos/UMLDemoStyle/1.0\" xmlns:compat=\"http://www.yworks.com/xml/yfiles-compat-arrows/1.0\" xmlns:GraphvizNodeStyle=\"http://www.yworks.com/yFilesHTML/graphviz-node-style/1.0\" xmlns:VuejsNodeStyle=\"http://www.yworks.com/demos/yfiles-vuejs-node-style/1.0\" xmlns:explorer-style=\"http://www.yworks.com/data-explorer/1.0\" xmlns:y=\"http://www.yworks.com/xml/yfiles-common/3.0\" xmlns:x=\"http://www.yworks.com/xml/yfiles-common/markup/3.0\" xmlns:yjs=\"http://www.yworks.com/xml/yfiles-for-html/2.0/xaml\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\n\t<key id=\"d0\" for=\"node\" attr.type=\"int\" attr.name=\"zOrder\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-z-order/1.0/zOrder\"/>\n\t<key id=\"d1\" for=\"node\" attr.type=\"boolean\" attr.name=\"Expanded\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/folding/Expanded\">\n\t\t<default>true</default>\n\t</key>\n\t<key id=\"d2\" for=\"node\" attr.type=\"string\" attr.name=\"url\"/>\n\t<key id=\"d3\" for=\"node\" attr.type=\"string\" attr.name=\"description\"/>\n\t<key id=\"d4\" for=\"node\" attr.name=\"NodeLabels\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/NodeLabels\"/>\n\t<key id=\"d5\" for=\"node\" attr.name=\"NodeGeometry\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/NodeGeometry\"/>\n\t<key id=\"d6\" for=\"all\" attr.name=\"UserTags\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/UserTags\"/>\n\t<key id=\"d7\" for=\"node\" attr.name=\"NodeStyle\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/NodeStyle\"/>\n\t<key id=\"d8\" for=\"node\" attr.name=\"NodeViewState\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/folding/1.1/NodeViewState\"/>\n\t<key id=\"d9\" for=\"edge\" attr.type=\"string\" attr.name=\"url\"/>\n\t<key id=\"d10\" for=\"edge\" attr.type=\"string\" attr.name=\"description\"/>\n\t<key id=\"d11\" for=\"edge\" attr.name=\"EdgeLabels\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/EdgeLabels\"/>\n\t<key id=\"d12\" for=\"edge\" attr.name=\"EdgeGeometry\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/EdgeGeometry\"/>\n\t<key id=\"d13\" for=\"edge\" attr.name=\"EdgeStyle\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/EdgeStyle\"/>\n\t<key id=\"d14\" for=\"edge\" attr.name=\"EdgeViewState\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/folding/1.1/EdgeViewState\"/>\n\t<key id=\"d15\" for=\"port\" attr.name=\"PortLabels\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/PortLabels\"/>\n\t<key id=\"d16\" for=\"port\" attr.name=\"PortLocationParameter\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/PortLocationParameter\">\n\t\t<default>\n\t\t\t<x:Static Member=\"y:FreeNodePortLocationModel.NodeCenterAnchored\"/>\n\t\t</default>\n\t</key>\n\t<key id=\"d17\" for=\"port\" attr.name=\"PortStyle\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/PortStyle\">\n\t\t<default>\n\t\t\t<x:Static Member=\"y:VoidPortStyle.Instance\"/>\n\t\t</default>\n\t</key>\n\t<key id=\"d18\" for=\"port\" attr.name=\"PortViewState\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/folding/1.1/PortViewState\"/>\n\t<key id=\"d19\" attr.name=\"SharedData\" y:attr.uri=\"http://www.yworks.com/xml/yfiles-common/2.0/SharedData\"/>\n\t<data key=\"d19\">\r\n\t\t<y:SharedData>\r\n\t\t\t<y:ExteriorLabelModel x:Key=\"1\" Insets=\"5\"/>\r\n\t\t\t<y:CompositeLabelModelParameter x:Key=\"2\">\r\n\t\t\t\t<y:CompositeLabelModelParameter.Parameter>\r\n\t\t\t\t\t<y:ExteriorLabelModelParameter Position=\"South\" Model=\"{y:GraphMLReference 1}\"/>\r\n\t\t\t\t</y:CompositeLabelModelParameter.Parameter>\r\n\t\t\t\t<y:CompositeLabelModelParameter.Model>\r\n\t\t\t\t\t<y:CompositeLabelModel>\r\n\t\t\t\t\t\t<y:CompositeLabelModel.LabelModels>\r\n\t\t\t\t\t\t\t<y:GraphMLReference ResourceKey=\"1\"/>\r\n\t\t\t\t\t\t\t<y:InteriorLabelModel/>\r\n\t\t\t\t\t\t\t<y:FreeNodeLabelModel/>\r\n\t\t\t\t\t\t</y:CompositeLabelModel.LabelModels>\r\n\t\t\t\t\t</y:CompositeLabelModel>\r\n\t\t\t\t</y:CompositeLabelModelParameter.Model>\r\n\t\t\t</y:CompositeLabelModelParameter>\r\n\t\t\t<yjs:DefaultLabelStyle x:Key=\"3\" verticalTextAlignment=\"BOTTOM\" horizontalTextAlignment=\"CENTER\" textFill=\"BLACK\">\r\n\t\t\t\t<yjs:DefaultLabelStyle.font>\r\n\t\t\t\t\t<yjs:Font fontSize=\"12\" fontFamily=\"'Arial'\"/>\r\n\t\t\t\t</yjs:DefaultLabelStyle.font>\r\n\t\t\t</yjs:DefaultLabelStyle>\r\n\t\t</y:SharedData>\r\n\t</data>");
         fprintf(f, "<graph id=\"G\" edgedefault=\"undirected\">\n");
+        // Add nodes to the graph
         for(auto i : g->nodes){
             Fieldpoint *p = i.data;
             fprintf(f, "\t<node id=\"%d\">\n", i.index);
             fprintf(f, "\t<data key=\"d5\">\n\t\t<y:RectD X=\"%d\" Y=\"%d\" Width=\"10\" Height=\"10\"/>\n\t</data>\n", p->x, p->y);
+            // Set node colour based on ownership
             switch(p->alliance){
                 case Alliance::RED:
                     fprintf(f, "\t<data key=\"d7\">\n\t\t<yjs:ShapeNodeStyle stroke=\"#FFFF0000\" fill=\"#FFFF0000\"/>\n\t\t</data>");
@@ -376,9 +403,11 @@ namespace Field {
                     fprintf(f, "\t<data key=\"d7\">\n\t\t<yjs:ShapeNodeStyle stroke=\"#FF4A2A7C\" fill=\"#FF4A2A7C\"/>\n\t\t</data>");
                     break;
             }
+            // Add index under node
             fprintf(f, "\t<data key=\"d4\">\n\t\t<x:List>\n\t\t\t<y:Label LayoutParameter=\"{y:GraphMLReference 2}\" Style=\"{y:GraphMLReference 3}\">\n\t\t\t\t<y:Label.Text>%d</y:Label.Text>\n\t\t\t</y:Label>\n\t\t</x:List>\n\t</data>\n\t", p->index);
             fprintf(f, "\t<port name=\"p0\"/>\n</node>\n");
         }
+        // Add edges between nodes
         for(auto i : g->adjacency) {
             LinkedList<Graph::Edge> *l = i.data;
             for(auto j : *l){
@@ -395,7 +424,10 @@ namespace Field {
     }
 
     void print(Graph *g, bool showdots){
+        // Array of nodes, has h/w of 1/10th size, each postion has 3 characters
         char out[FIELD_LENGTH/10][FIELD_WIDTH/10][3];
+
+        // Fill array with spaces, include dots if enabled
         for(int i=0; i<FIELD_WIDTH/10; i++){
             for(int j=0; j<FIELD_LENGTH/10; j++){
                 out[j][i][0]=showdots ? '.' : ' ';
@@ -405,6 +437,7 @@ namespace Field {
         }
 
         int x,y;
+        // For each node, update the apropriate position int he array with information
         for(int i = 0; i<g->nodes.size(); i++){
             x = g->nodes[i]->x/10;
             y = g->nodes[i]->y/10;
@@ -447,6 +480,7 @@ namespace Field {
             }
         }
 
+        // Print out the array
         for(int i=0; i<FIELD_WIDTH/10; i++){
             printf("%03d|",i*10);
             for(int j=0; j<FIELD_LENGTH/10; j++){
