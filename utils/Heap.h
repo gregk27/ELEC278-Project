ad933fd0192a7f6f0b193dc3cce6938ee96f8388
gregk27 <git@gregk.ca>
2020-11-30 02:04:03 -0500

    Added documentation to Heap.h

--- utils/Heap.h
+++ utils/Heap.h
@@ -1,3 +1,8 @@
+/**
+ * Heap.h
+ * This file contains code representing a heap
+*/
+
 #ifndef HEAP_H
 #define HEAP_H
 #include <vector>
@@ -8,28 +13,56 @@
 template <class T>
 class Heap{
     private:
+        /**
+         * Get the index of the left child
+         * - idx: The parent's index
+         * Returns: Left child's index
+        */
         int getLeft(int idx){
             return 2*idx+1;
         }
 
+        /**
+         * Get the index of the right child
+         * - idx: The parent's index
+         * Returns: Right child's index
+        */
         int getRight(int idx){
             return 2*idx+2;
         }
 
+        /**
+         * Get the index of the parent
+         * - idx: The child's index
+         * Returns: Parent's index
+        */
         int getParent(int idx){
             return (idx-1)/2;
         }
 
     public:
+        // Vector containing heap data
         std::vector<T> data;
+        // Function pointer used to compare 2 items
         std::function<bool(T, T)> comapre;
+        // Function pointer used to get string representation of item
         std::function<const char*(T)> toString;
 
+        /**
+         * Create a new heap
+         * - compare:  A function which takes 2 items, and returns true if the first item should be higher
+         * - toString: A function which takes an item, and returns the text representation 
+        */
         Heap<T>(std::function<bool(T, T)> comapre, std::function<const char*(T)> toString){
             this->comapre = comapre;
             this->toString = toString;
         }
 
+        /**
+         * Reheapify the list for a specific index
+         * This will work it's way up the heap, swaping as needed
+         * - idx: The index to start from
+        */
         void reHeapify(int idx){
             int parent;
             T d = data[idx];
@@ -47,12 +80,22 @@ class Heap{
             data[idx] = d;
         }
 
+        /**
+         * Push a new item onto the heap
+         * - d: The data to be added
+        */
         void push(T d){
             int idx = data.size();
             data.push_back(d);
             reHeapify(idx);
         }
 
+        /**
+         * Pop the item from the heap
+         * - out: Pointer to variable used to store result
+         * - idx: Index to pop from
+         * Returns: True if pop successful
+        */
         bool pop(T *out, int idx){
             if(data.size() == 0){
                 return false;
@@ -107,11 +150,21 @@ class Heap{
             return true;
         }
 
+        /**
+         * Peek at the value at an index
+         * - i: The index to peek at
+         * Returns: A pointer to the item at that index
+        */
         T *peek(int i){
             if(i >= data.size()) return NULL;
             return &data[i];
         }
 
+        /**
+         * Get the index of an item in the list
+         * - isEqual: Function to be used to compare items
+         * Returns: Index where isEqual returns true
+        */
         int indexOf(std::function<bool(T)> isEqual){
             for(int i=0; i<data.size(); i++){
                 if(isEqual(data[i])){
@@ -121,11 +174,12 @@ class Heap{
             return -1;
         }
 
+        /**
+         * Print whole tree. 
+         * We cannot make it look pretty graphically, so we add some characters to make it a little easier to understand. 
+         * - idx: The index to start from
+         */
         void printTree (int idx)
-        // Print whole tree. We cannot make it look pretty graphically, so we add some
-        // characters to make it a little easier to understand.  We also don't really
-        // know what the value field is - it is declared to be a void pointer - so we
-        // treat it as though it points to an integer.
         {
             // start of this tree
             printf("{");
